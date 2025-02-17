1f0985bcd032b56cb35e23faf3f3b9ccc4c9a6d3
jgobeill <48920425+jgobeill@users.noreply.github.com>
2025-02-17 14:34:38 +0100

    Update params.py

--- src/sibils2bioc/params.py
+++ src/sibils2bioc/params.py
@@ -5,5 +5,6 @@ list_fields = {"pmc": ["authors","affiliations"],
 flat_fields = {"pmc":["article_type","language","medline_ta","journal","title","pmid","doi","archive_id","manuscript_id","pmcid","publication_date","pmc_release_date","pubyear","issue","volume","start_page","end_page","medline_pgn","abstract","licence","subset"],
                "medline":["title","abstract","coi_statement","journal","pubyear","entrez_date","pmid","pmcid","doi","medline_ta"],
                "plazi":["treatment_title","zenodo-doi","treatment-bank-uri","article-title","publication-doi","nomenclature-taxon-name"],
-               "suppdata":["ext","pmcid","filename","licence","title","publication_date","pmc_release_date","pubyear"]
+               "suppdata":["ext","pmcid","filename","licence","title","publication_date","pmc_release_date","pubyear"],
+               "zenodo":["doi","status","title","journal","publisher","pub_date","resource_type","version_id","creators","subjects","licence","url","community"]
                }
